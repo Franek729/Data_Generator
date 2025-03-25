bf4963e3d5492b19e2722964422e00e9d598332a
oneballfranek <s193413@student.pg.edu.pl>
2025-03-25 12:57:53 +0100

    Created first instance Data Generator and possible data for data warehouse

---
+++ Data.py
@@ -0,0 +1,58 @@
+from Constants import *
+
+possible_names = [
+    # Męskie imiona
+    "Adam", "Adrian", "Aleksander", "Andrzej", "Antoni",
+    "Arkadiusz", "Artur", "Bartłomiej", "Bartosz", "Błażej",
+    "Bogdan", "Bolesław", "Cezary", "Cyprian", "Damian",
+    "Daniel", "Dariusz", "Dawid", "Dominik", "Edward",
+    "Emil", "Eryk", "Filip", "Franciszek", "Gabriel",
+    "Grzegorz", "Henryk", "Hubert", "Ignacy", "Ireneusz",
+    "Jacek", "Jakub", "Jan", "Janusz", "Jarosław",
+    "Jerzy", "Józef", "Kacper", "Kajetan", "Karol",
+    "Kazimierz", "Krzysztof", "Lech", "Leon", "Łukasz",
+    "Maciej", "Marek", "Marian", "Mariusz", "Mateusz",
+    # Damskie imiona
+    "Agnieszka", "Aleksandra", "Alicja", "Amelia", "Aneta",
+    "Anita", "Anna", "Barbara", "Beata", "Bogumiła",
+    "Bożena", "Celina", "Czesława", "Dagmara", "Danuta",
+    "Dorota", "Edyta", "Elżbieta", "Emilia", "Ewa",
+    "Gabriela", "Grażyna", "Halina", "Hanna", "Helena",
+    "Irena", "Izabela", "Jadwiga", "Janina", "Joanna",
+    "Jolanta", "Julia", "Justyna", "Kamila", "Karolina",
+    "Katarzyna", "Kazimiera", "Kinga", "Krystyna", "Laura",
+    "Lidia", "Lucyna", "Magdalena", "Małgorzata", "Maria",
+    "Marianna", "Marta", "Monika", "Natalia", "Nina"
+]
+
+possible_male_surnames = [ "Nowak", "Kowalski", "Wiśniewski", "Wójcik", "Kowalczyk",
+    "Kamiński", "Lewandowski", "Zieliński", "Szymański", "Woźniak",
+    "Dąbrowski", "Kozłowski", "Jankowski", "Mazur", "Wojciechowski",
+    "Kwiatkowski", "Krawczyk", "Kaczmarek", "Piotrowski", "Grabowski",
+    "Zając", "Pawłowski", "Michalski", "Król", "Wieczorek",
+    "Jabłoński", "Wróbel", "Nowakowski", "Majewski", "Olszewski",
+    "Stępień", "Malinowski", "Jaworski", "Adamczyk", "Dudek",
+    "Nowicki", "Pawlak", "Górski", "Witkowski", "Walczak",
+    "Sikora", "Baran", "Rutkowski", "Michalak", "Szewczyk",
+    "Ostrowski", "Tomaszewski", "Pietrzak", "Zalewski", "Wróblewski"]
+
+possible_female_surnames = ["Nowak", "Kowalska", "Wiśniewska", "Wójcik", "Kowalczyk",
+    "Kamińska", "Lewandowska", "Zielińska", "Szymańska", "Woźniak",
+    "Dąbrowska", "Kozłowska", "Jankowska", "Mazur", "Wojciechowska",
+    "Kwiatkowska", "Krawczyk", "Kaczmarek", "Piotrowska", "Grabowska",
+    "Zając", "Pawłowska", "Michalska", "Król", "Wieczorek",
+    "Jabłońska", "Wróbel", "Nowakowska", "Majewska", "Olszewska",
+    "Stępień", "Malinowska", "Jaworska", "Adamczyk", "Dudek",
+    "Nowicka", "Pawlak", "Górska", "Witkowska", "Walczak",
+    "Sikora", "Baran", "Rutkowska", "Michalak", "Szewczyk",
+    "Ostrowska", "Tomaszewska", "Pietrzak", "Zalewska", "Wróblewska"]
+
+possible_cities = ["Warszawa", "Kraków", "Łódź", "Gdańsk", "Poznań"]
+
+possible_streets = [ "ul. Marszałkowska", "ul. Krakowskie Przedmieście", "ul. Nowy Świat", "ul. Piotrkowska", "ul. Długa",
+    "ul. Świętokrzyska", "ul. Grójecka", "ul. Chmielna", "ul. Mokotowska", "ul. Puławska",
+    "ul. Jana Pawła II", "ul. Targowa", "ul. Złota", "ul. Żwirki i Wigury", "ul. Wspólna",
+    "Al. Jerozolimskie", "Al. Solidarności", "Al. Niepodległości", "Al. Wojska Polskiego", "Al. Wilanowska",
+    "Al. Rzeczypospolitej", "Al. Jana III Sobieskiego", "Al. KEN", "Al. Prymasa Tysiąclecia", "Al. Armii Ludowej",
+    "Al. Zieleniecka", "Al. Waszyngtona", "Al. Stanów Zjednoczonych", "Al. Krakowska", "Al. Wilanowska"]
+
---
+++ DataGenerator.py
@@ -0,0 +1,61 @@
+import random
+from datetime import datetime, timedelta
+from Data import *
+
+def generate_birth_date():
+    year = random.randint(1940, 2006)
+    start_date = datetime(year, 1, 1)
+    end_date = datetime(year, 12, 31)
+    time_between_dates = end_date - start_date
+    days_between_dates = time_between_dates.days
+    random_number_of_days = random.randint(0, days_between_dates)
+    random_date = start_date + timedelta(days=random_number_of_days)
+    return random_date
+
+def generate_pesel(birth_date, sex):
+    year = birth_date.year
+    month = birth_date.month
+    day = birth_date.day
+
+    if year >= 2000:
+        month += 20
+
+    serial = random.randint(0, 999)
+
+    gender_digit = random.choice([0, 2, 4, 6, 8]) if sex == 'F' else random.choice([1, 3, 5, 7, 9])
+
+    pesel = f"{year % 100:02d}{month:02d}{day:02d}{serial:03d}{gender_digit}"
+
+    weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+    checksum = sum(int(pesel[i]) * weights[i] for i in range(10)) % 10
+    checksum = (10 - checksum) % 10
+    pesel += str(checksum)
+
+    return pesel
+
+def generate_phone_number():
+    return f"48{random.randint(501000000, 819999999)}"
+
+def generate_customer():
+    name = random.choice(possible_names)
+    sex = 'F' if name[-1] == 'a' else 'M'
+    if sex == 'F':
+        surname = random.choice(possible_female_surnames)
+    else:
+        surname = random.choice(possible_male_surnames)
+    birth_date = generate_birth_date()
+    pesel = generate_pesel(birth_date, sex)
+    street = random.choice(possible_streets)
+    city = random.choice(possible_cities)
+    house_number = random.randint(1, 100)
+    return {
+        "name": name + ' ' + surname,
+        "birth_date": birth_date,
+        "pesel": pesel,
+        "phone_number": generate_phone_number(),
+        "city": city,
+        "address": street + ' ' + str(house_number),
+        "email": name.lower() + '.' + surname.lower() + '@gmail.com'
+    }
+
+print(generate_customer())
